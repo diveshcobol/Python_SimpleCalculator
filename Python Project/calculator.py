f084a087088f2286687c8c9f4ca2fe08833863de
divesh bari <diveshbari1999@gmail.com>
2026-02-23 17:03:09 +0530

    Added calculator

---
+++ Python Project/calculator.py
@@ -0,0 +1,16 @@
+# Simple Calculator Project in Python
+
+print("Simple Python Calculator")
+print("------------------------")
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("\nResults:")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
